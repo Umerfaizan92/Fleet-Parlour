4238e2fad07f065f2d6bbbfe10ef32ca7cbdd3df
Umerfaizan92 <umerfaizan@yahoo.com>
2026-09-12 23:55:00 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -331,7 +331,7 @@ document.addEventListener('DOMContentLoaded', function () {
     return new Intl.NumberFormat('en-AU').format(n);
   };
 
-  fetch('/api/social-stats', { headers: { Accept: 'application/json' } })
+      fetch('https://api.fleetparlour.com.au/api/social-stats', { headers: { Accept: 'application/json' } })    .then((response) => {
     .then((response) => {
       if (!response.ok) throw new Error('Social stats unavailable');
       return response.json();
